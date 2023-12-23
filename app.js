472c61c4f24a92054a130a20e5c878fc818162f4
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2023-12-22 19:15:42 -0700

    added comments to app.js to plan out the program

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// function for computer to randomly select choice
+
+// option for player to select choice
+
+// function that plays a single round
+
+// function to keep score for best of 5
+
+// rules for winning
+
+
+
+
+
 const currentTurn = 0;
 let playerTurn = 0;
 let computerTurn = 0;
