3f28807b5bb82749c3fd1a49aa735a92ef187178
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-01-05 15:13:29 -0700

    added string for computer or palyer winner.

--- app.js
+++ app.js
@@ -13,24 +13,24 @@ let computerSelection = function getComputerChoice() {
 
 let playerSelection = function getPlayerChoice() {
     return window.prompt('please select a choice: rock, paper, or scissors');
-    
+    //to Lower Case
 };
 
 // function that plays a single round
 
 function playRPS(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return 'rock beat scissors!'
+        return 'Player wins! rock beat scissors!'
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return 'scissors beat paper!'
+        return 'Player wins! scissors beat paper!'
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return 'paper beats rock!'
+        return 'Player wins! paper beats rock!'
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return 'rock beat scissors!'
+        return 'Computer wins! rock beat scissors!'
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return 'scissors beat paper!'
+        return 'Computer wins! scissors beat paper!'
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return 'paper beats rock!'
+        return 'Computer wins! paper beats rock!'
     } else return "it's a tie"
 };
 
